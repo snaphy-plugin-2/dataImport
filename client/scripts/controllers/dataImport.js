f484b8742e442a7e2987312c1c3da5e4628af8fd
ravi <ravigupta9363@gmail.com>
2017-02-27 11:59:16 +0530

    Work in progress for importing data of school

--- client/scripts/controllers/dataImport.js
+++ client/scripts/controllers/dataImport.js
@@ -7,6 +7,7 @@ angular.module($snaphy.getModuleName())
     function($scope, $stateParams, Database) {
         //Checking if default templating feature is enabled..
         var defaultTemplate = $snaphy.loadSettings('dataImport', "defaultTemplate");
+        $scope.state = $snaphy.loadSettings('dataImport', "state");
         $snaphy.setDefaultTemplate(defaultTemplate);
         //Use Database.getDb(pluginName, PluginDatabaseName) to get the Database Resource.
     }//controller function..
